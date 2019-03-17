b012930a6f0f2936aa76f1aaf78ed08086ebfe6f
Christian Benincasa <chrisbenincasa@gmail.com>
2019-03-17 15:24:42 -0600

    Remove scala

--- core/src/main/scala/com/chrisbenincasa/tools/sceleton/gen/mustache/CustomMustacheFactory.scala
+++
@@ -1,9 +0,0 @@
-package com.chrisbenincasa.tools.sceleton.gen.mustache
-
-import com.github.mustachejava.{DefaultMustacheFactory, ObjectHandler}
-
-class CustomMustacheFactory extends DefaultMustacheFactory {
-  private val objectHandler = new CustomObjectHandler
-
-  override def getObjectHandler: ObjectHandler = objectHandler
-}
--- core/src/main/scala/com/chrisbenincasa/tools/sceleton/util/package.scala
+++
@@ -1,5 +0,0 @@
-package com.chrisbenincasa.tools.sceleton
-
-package object util {
-  object Implicits extends TryExtensions with FileExtensions
-}
